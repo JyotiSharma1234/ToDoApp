6c7b358926636228ca03a39551c618d683db8efe
Jyoti Sharma <jyoti@joshsoftware.com>
2019-03-10 23:39:34 +0530

    Update ability

--- ToDoApp/app/models/ability.rb
+++ ToDoApp/app/models/ability.rb
@@ -3,19 +3,13 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    if user.admin?
+    if user.is_admin?
       can :manage, :all
     end
 
-    if user.developer?
+    if user.is_developer?
       can :manage, Task
       cannot :read, Project
-      can :manage, User, id: @user.id
-    end
-
-    if user.project_manager?
-      can :manage, Project
-      can :manage, Task
       can :manage, User, id: @user.id
     end
   end
