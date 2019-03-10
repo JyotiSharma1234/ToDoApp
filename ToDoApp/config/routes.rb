cf7079b125f61dc803d0e1fd9336bd3e9615ace5
Jyoti Sharma <jyoti@joshsoftware.com>
2019-03-10 23:26:08 +0530

    Routes for developers, projects, tasks

--- ToDoApp/config/routes.rb
+++ ToDoApp/config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :developers, controller: :developers
+  resources :tasks, controller: :tasks do
+    get :start
+    get :finish
+  end
+  resources :projects, controller: :projects
+  get 'users/dashboard'
+	devise_scope :user do
+    authenticated :user do
+      root 'users#dashboard', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 end
