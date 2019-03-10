2296ce1bc8826486d1f413c4bf1b2805007e82bc
Jyoti Sharma <jyoti@joshsoftware.com>
2019-03-10 23:37:43 +0530

    permit custom params

--- ToDoApp/app/controllers/application_controller.rb
+++ ToDoApp/app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :load_current_user
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :address])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :address])
+  end
+
+  def load_current_user
+  	@current_user = current_user
+  end
 end
