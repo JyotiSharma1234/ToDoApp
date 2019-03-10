8e1b20642b40e0762a5a1bbe9eb198231ce36042
Jyoti Sharma <jyoti@joshsoftware.com>
2019-03-10 23:35:52 +0530

    User CRUD

--- ToDoApp/app/models/user.rb
+++ ToDoApp/app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, format: {with: Devise.email_regexp}
+  validates :email, presence: :true, uniqueness: true
+  validates :name, presence: :true
+
+	def is_admin?
+    type == 'Admin'
+  end
+
+  def is_developer?
+    type == 'Developer'
+  end
+
+  def self.search(search)
+    where("name LIKE ?", "%#{search}%").order("created_at DESC")
+  end
 end
